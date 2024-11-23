712ca43e159bc5ad246b312524573f98604de0df
Rahul Shah <rahulshah0728@gmail.com>
2024-11-23 23:55:31 +0530

    Added spinner in all service 00:00 AM

--- src/components/SendToken.jsx
+++ src/components/SendToken.jsx
@@ -1,5 +1,6 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
 import { LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction} from "@solana/web3.js";
+import { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -8,7 +9,7 @@ export function SendTokens() {
     const wallet = useWallet();
     const {connection} = useConnection();
     const notify = (meassage) => toast(meassage);
-
+    const [loading,setLoading] = useState(false);
 
     async function sendTokens() {
         let to = document.getElementById("to").value;
@@ -20,10 +21,12 @@ export function SendTokens() {
                 toPubkey: new PublicKey(to),
                 lamports: amount * LAMPORTS_PER_SOL,
             }));
-    
+            setLoading(true);
             await wallet.sendTransaction(transaction, connection);
+            setLoading(false);
             notify("Successfully sent " + amount + " SOL to " + to);
         } catch (e) {
+            setLoading(false);
             notify("Failed to send " + amount + " SOL to " + to + ": " + e.message);
         }
     }
@@ -47,6 +50,9 @@ export function SendTokens() {
         position="bottom-right"
         hideProgressBar={false}
         />
+        {loading === true ? <div className="fixed top-0 left-0 z-50 w-screen h-screen bg-black bg-opacity-50 flex justify-center items-center">
+        <div className="animate-ping rounded-full h-20 w-20 border-8 border-[#FF6500]"></div>
+        </div> : null}
     </div>
     )
 }
--- src/components/SignMessage.jsx
+++ src/components/SignMessage.jsx
@@ -1,13 +1,14 @@
 import { ed25519 } from '@noble/curves/ed25519';
 import { useWallet } from '@solana/wallet-adapter-react';
 import bs58 from 'bs58';
+import { useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export function SignMessage() {
     const { publicKey, signMessage } = useWallet();
     const notify = (meassage) => toast(meassage);
-
+    const [loading,setLoading] = useState(false);
 
     async function onSignMessage() {
         if (!publicKey) throw new Error('Wallet not connected!');
@@ -16,11 +17,13 @@ export function SignMessage() {
         try {
             const message = document.getElementById("message").value;
             const encodedMessage = new TextEncoder().encode(message);
+            setLoading(true);
             const signature = await signMessage(encodedMessage);
-    
+            setLoading(false);
             if (!ed25519.verify(signature, encodedMessage, publicKey.toBytes())) throw new Error('Message signature invalid!');
             notify(`Message signature: ${bs58.encode(signature)}`);
         } catch (error) {
+            setLoading(false);
             notify("Failed to sign message: " + error.message)
         }
     };
@@ -37,6 +40,9 @@ export function SignMessage() {
             position="bottom-right"
             hideProgressBar={false}
             />
+            {loading === true ? <div className="fixed top-0 left-0 z-50 w-screen h-screen bg-black bg-opacity-50 flex justify-center items-center">
+            <div className="animate-ping rounded-full h-20 w-20 border-8 border-[#FF6500]"></div>
+            </div> : null}
         </div>
     );
 };
